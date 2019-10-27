5dc4c670fe238ef2e84250ec5f94c3c00802be5a
Clare Zhu <clarezhu@stanford.edu>
2019-10-27 18:58:42 -0400

    Temporal progress

---
+++ temporal.py
@@ -0,0 +1,27 @@
+import networkx as nx
+import os
+
+
+class TemporalGraph:
+    
+    def __init__(self, node_ordering, index):
+        self.frames = {}
+        self.index = index
+        self.node_ordering = node_ordering
+    
+    def add_frame(self, ind, frame):
+        self.frames[ind] = frame
+        
+    def get_frame(self, ind):
+        """ Returns a copy of the frame at the index.
+        """
+        return self.frames[ind].copy()
+        
+    def read_gpickles(self, directory):
+        for ind_path in os.listdir(directory):
+            ind = os.path.basename(ind_path).split(".")[0]
+            self.frames[ind] = nx.read_gpickle(ind_path)
+            
+    def write_gpickles(self, directory):
+        for ind, frame in self.frames.items():
+            nx.write_gpickle(frame, os.path.join(directory, "%s.pkl" % ind))
